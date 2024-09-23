b021d6f9829d2ce557c58d41d2b6c36732df4f00
sangitakumari7 <sangitaroy4829@gmail.com>
2024-09-23 20:00:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const numberInput = document.getElementById("numberInput");
+    const checkButton = document.getElementById("checkButton");
+    const resultMessage = document.getElementById("resultMessage");
+
+    checkButton.addEventListener("click",function(){
+        const number = parseInt(numberInput.value);
+        if(isNaN(number)){
+            resultMessage.textContent = "please enter a valid number";
+            resultMessage.style.color = "red";
+            return;
+        }
+        if(isPrime(number)){
+            resultMessage.textContent = `${number} is a prime number`
+            resultMessage.style.color = "green";
+
+        }
+        else{
+            resultMessage.textContent = `${number} is a prime number`
+            resultMessage.style.color = "red";
+        }
+    });
+
+    function isPrime(num){
+        if(num <= 1) return false;
+        if (num <=3) return true;
+
+        if(num % 2 === 0 || num % 3 === 0) return false;
+
+        return true;
+    }
+});
